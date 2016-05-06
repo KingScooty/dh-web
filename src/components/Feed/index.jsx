11b99e1200cad0a2f350d38a04a93dd0aeadcac3
Scotty Vernon <scott.vernon@gmail.com>
2016-05-06 19:38:56 +0100

    Flesh out that Feed.

--- src/components/Feed/index.jsx
+++ src/components/Feed/index.jsx
@@ -13,8 +13,26 @@ var Feed = React.createClass({
     var feed = this.getFeed();
 
     return (
-      <div>
-        {feed}
+      <div className="stream">
+        <div className="stream__header">
+
+          <div className="live-status">LIVE</div>
+
+          <ul className="stream-navigation">
+            <li className="stream-navigation__item">2016</li>
+            <li className="stream-navigation__item">2015</li>
+            <li className="stream-navigation__item">2014</li>
+          </ul>
+
+        </div>
+
+        <div className="stream__body">
+          <div className="stream-post">
+            <div className="stream-post__meta">5 months ago</div>
+            <div className="stream-body">This is a dummy tweet. Lolz.</div>
+          </div>
+
+        </div>
       </div>
     );
   }
