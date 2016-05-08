958bfbc37e802c65861c616c536f6a31889955ac
Scotty Vernon <scott.vernon@gmail.com>
2016-05-08 15:23:11 +0100

    More post media tests.

--- src/components/Post/PostMedia.jsx
+++ src/components/Post/PostMedia.jsx
@@ -6,24 +6,33 @@ var PostMedia = React.createClass({
     return <img {...props} className="stream-post__media-item" />
   },
 
+  renderMedia: function() {
+    return (
+      <div className="stream-post__media">
+        <a href={this.props.href}>
+          { this.getMedia() }
+        </a>
+      </div>
+    )
+  },
+
   getMedia: function() {
 
     if (this.props.extended_entities && this.props.extended_entities.media) {
       let media = this.props.extended_entities.media;
 
-      return media.map(function(media, index) {
+      return media.map((media, index) => {
         return this.imgElement({ key: index, src: media.media_url});
       });
 
-    } else if (this.props.media && this.props.media.media_url) {
-      let media = this.props.media;
+    } else if (this.props.entities && this.props.entities.media) {
+      let media = this.props.entities.media;
 
-      return media.map(function(media, index) {
+      return media.map((media, index) => {
         return this.imgElement({ key: index, src: media.media_url});
       });
 
     } else {
-
       let media = this.props.media;
       return this.imgElement({ src: media });
 
@@ -31,7 +40,6 @@ var PostMedia = React.createClass({
   },
 
   render: function() {
-
     return (
       <div className="stream-post__media">
         <a href={this.props.href}>
--- src/components/Post/__specs__/postMedia-spec.js
+++ src/components/Post/__specs__/postMedia-spec.js
@@ -10,6 +10,7 @@ import PostMedia from '../PostMedia';
 var legacyMedia = require('./mocks/tweet_with_media--simple.json');
 var media2014 = require('./mocks/tweet_2014_with_media.json');
 var mediaModern = require('./mocks/tweet_2015_with_media.json');
+var mediaModernMultiple = require('./mocks/tweet_2015_with_multiple_media.json');
 
 // function mockMedia(mediaType) {
   // var mediaObject = {};
@@ -28,7 +29,6 @@ describe('<PostMedia />', function() {
       const wrapper = shallow(<PostMedia {...legacyMedia} />);
       const image = wrapper.find('.stream-post__media-item');
 
-      // expect(image.prop('src')).to.equal(post.profile_image_url);
       expect(image).to.have.length.of(1);
     });
 
@@ -42,9 +42,68 @@ describe('<PostMedia />', function() {
   });
 
   describe('2014 data format', function() {
+    // it('renders no media object if no media present', function() {
+    //   const wrapper = shallow(<PostMedia {...media2014} />);
+    //   const image = wrapper.find('.stream-post__media-item');
+    //
+    //   expect(image).to.have.length.of(0);
+    // });
+
+    it('renders a single media object if media present', function() {
+      const wrapper = shallow(<PostMedia {...media2014} />);
+      const image = wrapper.find('.stream-post__media-item');
+
+      expect(image).to.have.length.of(1);
+    });
+
+    it('renders the media object with correct props', function() {
+      const wrapper = shallow(<PostMedia {...media2014 } />);
+      const image = wrapper.find('.stream-post__media-item');
+      const expectedSrc = media2014.entities.media[0].media_url;
+
+      expect(image.prop('src')).to.equal(expectedSrc);
+    });
   });
 
   describe('>= 2015 (modern data format)', function() {
+    // it('renders no media object if no media present', function() {
+    //   const wrapper = shallow(<PostMedia {...media2014} />);
+    //   const image = wrapper.find('.stream-post__media-item');
+    //
+    //   expect(image).to.have.length.of(0);
+    // });
+
+    it('renders a single media object if media present', function() {
+      const wrapper = shallow(<PostMedia {...mediaModern} />);
+      const image = wrapper.find('.stream-post__media-item');
+
+      expect(image).to.have.length.of(1);
+    });
+
+    it('renders the media object with correct props', function() {
+      const wrapper = shallow(<PostMedia {...mediaModern } />);
+      const image = wrapper.find('.stream-post__media-item');
+      const expectedSrc = mediaModern.extended_entities.media[0].media_url;
+
+      expect(image.prop('src')).to.equal(expectedSrc);
+    });
+
+    it('renders multiple media objects if multiple media present', function() {
+      const wrapper = shallow(<PostMedia {...mediaModernMultiple} />);
+      const images = wrapper.find('.stream-post__media-item');
+
+      expect(images).to.have.length.of(3);
+    });
+
+    it('renders the media object with correct props', function() {
+      const wrapper = shallow(<PostMedia {...mediaModernMultiple } />);
+      const images = wrapper.find('.stream-post__media-item');
+      const expectedSrc = mediaModernMultiple.extended_entities.media;
+
+      images.forEach(function(image, index) {
+        expect(image.prop('src')).to.equal(expectedSrc[index].media_url);
+      });
+    });
   });
 
 });
