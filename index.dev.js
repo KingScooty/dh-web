9734514a2fc369d4ba148ec18581cffa70cad5be
Scotty Vernon <scott.vernon@gmail.com>
2016-05-14 16:20:45 +0100

    Hot reloading now in place!

--- index.dev.js
+++ index.dev.js
@@ -34,5 +34,25 @@ const getServer = () => {
 };
 
 getServer().listen(3000, () => {
-  console.log('HTTP server listening on port 1337');
+  console.log('HTTP server listening on port 3000');
+});
+
+
+// we start a webpack-dev-server with our config
+var webpack = require('webpack');
+var WebpackDevServer = require('webpack-dev-server');
+var config = require('./webpack.config.js');
+
+new WebpackDevServer(webpack(config), {
+  hot: true,
+  historyApiFallback: true,
+  proxy: {
+   "*": "http://localhost:3000"
+  }
+}).listen(3001, 'localhost', function (err, result) {
+  if (err) {
+   console.log(err);
+  }
+
+  console.log('Listening at localhost:3001');
 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,20 @@
-
+var path = require('path');
+var webpack = require('webpack');
 
 module.exports = {
-  entry: './src/client.js',
+  // entry: './src/client.js',
+  entry: [
+    'webpack-dev-server/client?http://127.0.0.1:3001/',
+    'webpack/hot/only-dev-server',
+    './src/client.js'
+  ],
   output: {
-    filename: './src/dist/bundle.js'
+    // filename: './src/dist/bundle.js'
+    path: path.join(__dirname, 'src/dist'),
+    filename: 'bundle.js'
   },
   resolve: {
+    modulesDirectories: ['node_modules'],
     extensions: ['', '.js', '.jsx']
   },
   module: {
@@ -13,20 +22,24 @@ module.exports = {
       {
         test: /\.jsx?$/,
         exclude: /(node_modules|bower_components)/,
-        loader: 'babel', // 'babel-loader' is also a legal name to reference
-        query: {
-          presets: ['es2015', 'react']
-        }
+        // loader: 'babel', // 'babel-loader' is also a legal name to reference
+        loaders: ['react-hot', 'babel'] // ,
+        // query: {
+        //   presets: ['es2015', 'react']
+        // }
       }
     ]
-    // loaders: [
-    //   {
-    //     test: /\.jsx?$/,
-    //     loader: 'babel',
-    //     query: {
-    //       presets: ['react', 'es2015']
-    //     }
-    //   }
-    // ]
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ],
+  devtool: 'inline-source-map',
+  devServer: {
+    hot: true,
+    proxy: {
+      '*': 'http://127.0.0.1:' + (process.env.PORT) || 1337
+    },
+    host: '127.0.0.1'
   }
 };
