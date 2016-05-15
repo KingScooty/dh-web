a8028dfc11ba946f547dc086f3d7199eef628f0b
Scotty Vernon <scott.vernon@gmail.com>
2016-05-15 11:05:10 +0100

    Tinkering with routes. Added 2 new containers.

---
+++ README.md
@@ -0,0 +1,15 @@
+-App
+  - Home *(TBD: Controlled by route)*
+    - EventInfo **(Controlled by route)**
+    - Stream
+      - Navigation
+      - LiveFeed **(Controlled by route)**
+        - Post
+      - ArchiveFeed **(Controlled by route)**
+        - Post
+  - About [?] *(TBD: Controlled by route)*
+
+
+## Stateless components
+Write the presentational components that lack state as simpler, functional components:
+https://facebook.github.io/react/blog/2015/10/07/react-v0.14.html#stateless-functional-components
--- src/routes.js
+++ src/routes.js
@@ -1,22 +1,29 @@
 // modules/routes.js
 import React from 'react'
 import { Route, IndexRoute } from 'react-router'
-import Root from './containers/Root'
+import App from './App'
+import Home from './containers/Home'
 // import About from './About'
 // import Repos from './Repos'
 // import Repo from './Repo'
 // import Home from './Home'
 
 module.exports = (
-  <Route path="/" component={Root}>
+  <Route name="layout" path="/" component={App} ignoreScrollBehavior>
     {/*<IndexRedirect to="/welcome" />*/}
-    <IndexRoute component={Home}/>
-    <Route path="/repos" component={Repos}>
+    {/*<IndexRoute component={Home}/>*/}
+    {/*<Route path="/repos" component={Repos}>
       <Route path="/repos/:userName/:repoName" component={Repo}/>
-    </Route>
+    </Route>*/}
+    <Route name="live" path="/halloween15" component={LiveFeed}/>
+    <Route name="year" path="/:year" component={ArchiveFeed}/>
   </Route>
 );
-
+//
+// <Route name="layout" path="/" handler={App} ignoreScrollBehavior>
+//   <Route name="live" path="/halloween15" handler={LiveFeed}/>
+//   <Route name="year" path="/:year" handler={ArchiveFeed}/>
+// </Route>
 
 
 //
