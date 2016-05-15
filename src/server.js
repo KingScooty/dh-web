034cacb0be3885c658908374d270b24c1459fa23
Scotty Vernon <scott.vernon@gmail.com>
2016-05-15 18:51:07 +0100

    Await next incase new middleware is added.

--- src/server.js
+++ src/server.js
@@ -1,23 +1,22 @@
 'use strict';
-// const Promise = require('bluebird');
-// const co = Promise.coroutine;
+
 const path = require('path');
-const register = require('babel-register');
 const compress = require('koa-compress');
 const morgan = require('koa-morgan');
-// const react = require('koa-react-view');
-// middleware
 
+const React = require('react');
+const ReactDOMServer = require('react-dom/server');
+const Router = require('react-router');
+const RouterContext = Router.RouterContext;
+
+// middleware
 const Koa = require('koa');
 const web = new Koa();
 
 const logger = morgan('combined');
-// const srcPath = __dirname;
-// const viewpath = path.join(__dirname, 'views');
-// const containerPath = path.join(__dirname, 'containers');
-// const componentpath = path.join(__dirname, 'components');
-
 const serve = require('koa-static');
+
+const routes = require('./routes');
 
 web.use(serve(path.join(__dirname, 'dist')));
 
@@ -25,81 +24,7 @@ web.use(compress({
   flush: require('zlib').Z_SYNC_FLUSH
 }));
 
-// react(web,
-//   {
-//     views: viewpath,
-//     internals: true
-//   }
-// );
-
-// // imports babel runtime for JSX views, warning: live transpiling
-// // best to precompile in production deploys for perf + reliability
-// register({
-//   only: [
-//     srcPath
-//     // viewpath,
-//     // containerPath,
-//     // componentpath
-//   ]
-// });
-
-// const config = require('./config/');
-// const errorMiddleware = require('./middleware/errors');
-// web.use(errorMiddleware());
-
-// const router = require('./routes/')(web);
-// router(web);
-// const router = require('./routes');//(web);
-// router(web);
-
-// web
-//   .use(router.routes())
-//   .use(router.allowedMethods());
-
 web.use(logger);
-
-// var tweetData = require('./components/Post/__specs__/mocks/tweet_2015_with_multiple_media.json');
-
-// web.use(function* () {
-//   this.render('index', {
-//     title: 'List',
-//     list: [
-//       'hello koa',
-//       'hello react'
-//     ],
-//     posts: [tweetData, tweetData, tweetData]
-//   });
-// });
-
-var React = require('react');
-var ReactDOMServer = require('react-dom/server');
-var Router = require('react-router');
-var routes = require('./routes');
-
-var RouterContext = Router.RouterContext;
-
-// web.use(function *() {
-//   let appHtml;
-//
-//   Router.match({
-//     routes: routes,
-//     location: this.url
-//   }, (error, redirectLocation, renderProps) => {
-//     if (error) {
-//       this.throw(error.message, 500);
-//     } else if (redirectLocation) {
-//       this.redirect(redirectLocation.pathname + redirectLocation.search);
-//     } else if (renderProps) {
-//       appHtml = ReactDOMServer.renderToString(<RouterContext {...props} />);
-//     }
-//
-//     let renderString = renderPage(appHtml);
-//
-//     this.body = renderString;
-//   });
-// });
-
-let handler;
 
 web.use(async function(ctx, next) {
   let appHtml;
@@ -109,7 +34,6 @@ web.use(async function(ctx, next) {
     location: ctx.url
   },
   (error, redirectLocation, renderProps) => {
-
     if (error) {
       ctx.throw(error.message, 500);
     }
@@ -120,14 +44,9 @@ web.use(async function(ctx, next) {
       appHtml = ReactDOMServer.renderToString(<RouterContext {...renderProps} />);
       ctx.body = appHtml;
     }
-
   });
-  // await next();
+  await next();
 });
-
-// web.use(async ctx => {
-//   ctx.body = handler;
-// });
 
 function renderPage(renderedBody) {
   let html = `
@@ -147,9 +66,5 @@ function renderPage(renderedBody) {
 
   return html;
 }
-
-// web
-//   .use(router.routes())
-//   .use(router.allowedMethods());
 
 module.exports = web;
