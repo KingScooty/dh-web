9688cace4c821f37028f70bacb78a9277870711f
Scotty Vernon <scott.vernon@gmail.com>
2016-05-17 20:39:27 +0100

    Wire up navigation to router.

--- src/components/Navigation/index.jsx
+++ src/components/Navigation/index.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 var Navigation = React.createClass({
+
   // handleClick: function(name) {
   //   if (window._gs) {
   //     // console.log(event);
@@ -14,9 +16,24 @@ var Navigation = React.createClass({
   render: function () {
     return (
       <ul className="stream-navigation">
-        <li className="stream-navigation__item">2016</li>
-        <li className="stream-navigation__item">2015</li>
-        <li className="stream-navigation__item">2014</li>
+        <li className="stream-navigation__item">
+          <Link to="/2016">2016</Link>
+        </li>
+        <li className="stream-navigation__item">
+          <Link to="/halloween15">Halloween ’15</Link>
+        </li>
+        <li className="stream-navigation__item">
+          <Link to="/2015">2015</Link>
+        </li>
+        <li className="stream-navigation__item">
+          <Link to="/2014">2014</Link>
+        </li>
+        <li className="stream-navigation__item">
+          <Link to="/2013">2013</Link>
+        </li>
+        <li className="stream-navigation__item">
+          <Link to="/2012">2012</Link>
+        </li>
       </ul>
     );
   }
