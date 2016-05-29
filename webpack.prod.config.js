d606ea008edcbd1bfcf87e182c67660935c39aa8
Scotty Vernon <scott.vernon@gmail.com>
2016-05-29 17:58:00 +0100

    Setup CI.

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,28 +1,28 @@
-// var path = require('path');
-//
+var path = require('path');
+
 // var outDirectory = (process.env.NODE_ENV === 'production') ?
 //   'dist' :
 //   'build';
-//
-// module.exports = {
-//   entry: [
-//     './client.js'
-//   ],
-//   resolve: {
-//     modulesDirectories: ['node_modules', 'shared'],
-//     extensions: ['', '.js', '.jsx']
-//   },
-//   output: {
-//     path: path.join(__dirname, outDirectory),
-//     filename: 'bundle.js'
-//   },
-//   module: {
-//     loaders: [
-//       {
-//         test: /\.jsx?$/,
-//         exclude: /node_modules/,
-//         loaders: ['babel']
-//       }
-//     ]
-//   }
-// };
+
+module.exports = {
+  entry: [
+    './src/client.js'
+  ],
+  resolve: {
+    modulesDirectories: ['node_modules', 'shared'],
+    extensions: ['', '.js', '.jsx']
+  },
+  output: {
+    path: path.join(__dirname, 'src/dist/'),
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loaders: ['babel']
+      }
+    ]
+  }
+};
