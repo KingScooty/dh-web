30aa3ee8a1a4ed7a7ef28f6266c6406c8759e99b
Scotty Vernon <scott.vernon@gmail.com>
2016-05-30 21:50:21 +0100

    Wire up event info to redux state.

--- src/containers/LiveIndicator/index.jsx
+++ src/containers/LiveIndicator/index.jsx
@@ -1,4 +1,4 @@
-const React = require('react');
+import React from 'react';
 import { connect } from 'react-redux';
 
 var LiveIndicator = require('../../components/LiveIndicator');
--- src/containers/Page/Home.jsx
+++ src/containers/Page/Home.jsx
@@ -1,25 +1,31 @@
 'use strict';
 
 import React from 'react';
+import { connect } from 'react-redux';
 
 const Logo = require('../../components/Logo');
 const EventInfo = require('../../components/EventInfo');
 
 var Home = React.createClass({
   propTypes: {
+    titleLead: React.PropTypes.string,
+    titleSupport: React.PropTypes.string,
+    eventTag: React.PropTypes.string,
+    body: React.PropTypes.string,
+    footer: React.PropTypes.string,
     Stream: React.PropTypes.node.isRequired
   },
 
   render: function () {
     var logoProps = {
-      title: 'BLACK OPS HEROES',
-      eventTag: 'blackops-2016'
+      title: this.props.titleLead,
+      eventTag: this.props.eventTag
     };
     var eventInfoProps = {
-      titleLead: 'BLACK OPS HEROES',
-      titleSupport: 'DEV\'ING THIS BITCH  UP SINCE 2012',
-      body: `Digital heroes does Halloween! The one in the summer was so good, we\'re doing it again, for Halloween! \n\n As per Digital Heroes tradition; a fancy dress night out with all my closest friends &amp; wonderful Web/digital folk of Manchester. (*Fancy dress is a __must__ this time! :P*) - *[see previous years below for ideas.](#twitter-stream)* \n\n The plan is as follows: \n\n ## 18:00, SAT 31ST @ TBD, NQ \n\n Plan is hazy at the moment. Liverpool play Chelsea in the afternoon, so the festivities could potentially start as early as 12:45pm! All i know at the moment is we'll be off to Satan's Hollow in the evening. \n\n ## REALTIME DIARY OF THE EVENT \n\n Leave a tweet with the hashtag *#digitalheroes2015* or *#halloweenheroes2015* and your message (including images) will be pulled in below in realtime. Let's get a diary of the build up, and of the night itself! :-)`,
-      footer: 'LIVE TWITTER FEED – #digitalheroes2015 / #halloweenheroes2015'
+      titleLead: this.props.titleLead,
+      titleSupport: this.props.titleSupport,
+      body: this.props.body,
+      footer: this.props.footer
     };
 
     var Stream = this.props.Stream;
@@ -34,4 +40,17 @@ var Home = React.createClass({
   }
 });
 
-module.exports = Home;
+var mapStateToProps = function (state) {
+  const eventInfo = state.events.eventInfo;
+  return {
+    titleLead: eventInfo.titleLead,
+    titleSupport: eventInfo.titleSupport,
+    eventTag: eventInfo.eventTag,
+    body: eventInfo.body,
+    footer: eventInfo.footer
+  };
+};
+
+module.exports = connect(
+  mapStateToProps
+)(Home);
