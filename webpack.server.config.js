5957b15cf9219f7441a67faad1b4283e1e676762
Scotty Vernon <scott.vernon@gmail.com>
2016-06-06 09:45:13 +0100

    Add a server webpack conf.

---
+++ webpack.server.config.js
@@ -0,0 +1,31 @@
+var path = require('path');
+var webpack = require('webpack');
+
+var nodeExternals = require('webpack-node-externals');
+
+module.exports = {
+  entry: './index.js',
+  target: 'node',
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'server.js'
+  },
+  externals: [nodeExternals()],
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loaders: ['babel']
+      }
+    ]
+  },
+  plugins: [
+    new webpack.IgnorePlugin(/\.(css|less)$/),
+    new webpack.BannerPlugin({banner: 'require("source-map-support").install();', raw: true, entryOnly: false })
+  ],
+  devtool: 'sourcemap'
+};
