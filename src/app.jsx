92f51e4a1e0d40f903cad84ac7652754dfcb0cd2
Scotty Vernon <scott.vernon@gmail.com>
2016-06-17 23:48:57 +0100

    Split out CSS to correct component and add images with config.

--- src/app.jsx
+++ src/app.jsx
@@ -3,6 +3,10 @@
 import React from 'react';
 // var Home = require('./containers/Home');
 
+if (typeof window !== 'undefined') {
+  require('./static/sass/main.scss');
+}
+
 var App = React.createClass({
   propTypes: {
     children: React.PropTypes.node // .isRequired
