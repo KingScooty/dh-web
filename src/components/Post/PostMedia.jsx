b01ed7b8ac16b6f340c0b3e68ade1fa2e689ee72
Scotty Vernon <scott.vernon@gmail.com>
2016-07-02 01:36:01 +0100

    Handle instagram images

--- src/components/Post/PostMedia.jsx
+++ src/components/Post/PostMedia.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import classNames from 'classnames';
-import PostMediaTwitter from './PostMediaTwitter';
 
-var PostMedia = React.createClass({
+import * as mediaHelpers from './mediaHelpers';
+import PostMediaTwitter from './PostMediaTwitter';
+import PostMediaInstagram from './PostMediaInstagram';
+
+const PostMedia = React.createClass({
   propTypes: {
     entities: React.PropTypes.shape({
+      urls: React.PropTypes.array.isRequired,
       media: React.PropTypes.arrayOf(
         React.PropTypes.shape({
           media_url: React.PropTypes.string.isRequired
@@ -22,10 +26,21 @@ var PostMedia = React.createClass({
     href: React.PropTypes.string
   },
 
+  instagram: function () {
+    const images = mediaHelpers.isInstagram(this.props.entities.urls);
+    if (!images.length) return;
+
+    const shortcodes = mediaHelpers.getShortKeys(images);
+    return shortcodes.map(function (shortcode, index) {
+      return <PostMediaInstagram shortcode={ shortcode } key={ index } />;
+    });
+  },
+
   render: function () {
     // const media = this.getMedia();
     // if (!media) return false;
 
+    // Need to hide this media div if there is no media!!
     return (
       <div className="stream-post__media">
         <PostMediaTwitter
@@ -33,6 +48,7 @@ var PostMedia = React.createClass({
           extended_entities={ this.props.extended_entities }
           href={ this.props.href }
           />
+          { this.instagram() }
       </div>
     );
   }
--- src/components/Post/PostMediaInstagram.jsx
+++ src/components/Post/PostMediaInstagram.jsx
@@ -1,18 +1,50 @@
-import React, { Component, PropTypes } from 'react';
-// import LazyLoadFade from '../_shared/LazyLoadFade';
-// import ResponsiveImage from '../_shared/ResponsiveImage';
-// import VerticalRhythm from '../_shared/VerticalRhythm';
+import React, { createClass, PropTypes } from 'react';
 
-const getInstagramImage = function getInstagramImage(shortcode) {
-  return `https://instagram.com/p/${shortcode}/media/?size=l`;
-};
+import LazyLoadFade from '../_shared/LazyLoadFade';
+import ResponsiveImage from '../_shared/ResponsiveImage';
+import VerticalRhythm from '../_shared/VerticalRhythm';
 
-class PostMediaInstagram extends Component {
-  render() {
+const PostMediaInstagram = createClass({
+  propTypes: {
+    shortcode: PropTypes.string.isRequired// ,
+    // href: PropTypes.string.isRequired
+  },
+
+  defineSizes: function defineSizes() {
+    return {
+      medium: {
+        w: 600,
+        h: 600,
+        resize: 'fit'
+      }
+    };
+  },
+
+  getInstagramImage: function getInstagramImage(shortcode) {
+    return `https://instagram.com/p/${shortcode}/media/?size=l`;
+  },
+
+  render: function render() {
+    const sizes = this.defineSizes();
+    const shortcode = this.props.shortcode;
+    const imgUrl = this.getInstagramImage(shortcode);
+
+    return (
+      <div className="stream-post__media-item">
+        <a href={ `https://www.instagram.com/p/${shortcode}/` }>
+          <div className="u-crop-rounded">
+            <VerticalRhythm>
+              <ResponsiveImage { ...sizes }>
+                <LazyLoadFade>
+                  <img src={ imgUrl } />
+                </LazyLoadFade>
+              </ResponsiveImage>
+            </VerticalRhythm>
+          </div>
+        </a>
+      </div>
+    );
   }
-}
-
-PostMediaInstagram.propTypes = {
-};
+});
 
 export default PostMediaInstagram;
