53b2ae2c9b490f690e929ee6ea0b3c231e44c8bd
Scotty Vernon <scott.vernon@gmail.com>
2017-12-03 20:58:28 +0000

    start setting up tests

---
+++ components/EventInfo/__test__/eventInfo.spec.js
@@ -0,0 +1,27 @@
+/* eslint-env jest */
+
+import React from 'react';
+import { shallow, render } from 'enzyme';
+// import sinon from 'sinon';
+// import sinonChai from 'sinon-chai';
+
+import EventInfo from '../';
+
+// chai.use(sinonChai);
+
+test('returns nothing if there is no body prop', function () {
+  const response = EventInfo.prototype.convertMarkdown();
+  expect(response).to.equal(false);
+});
+
+test('returns a sanitized markdown repsonse if body prop exists', function () {
+  const markdown = '# This is a title';
+  const response = EventInfo.prototype.convertMarkdown(markdown);
+
+  expect(response.dangerouslySetInnerHTML.__html).to.equal('<h1 id="this-is-a-title">This is a title</h1>\n');
+});
+
+// describe('<EventInfo />', function () {
+//   describe('convertMarkdown()', function () {
+//   });
+// });
